743a7f1d32d8f5559676262d3a03a7bf7dd283b5
Shunya Nagahori <nagahori.shunya@gmail.com>
2023-11-21 21:19:43 +0900

    create pdf button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@
 import React, { FormEvent, useRef, useState } from 'react';
 import LineChart from '@/app/components/LineChart';
 import BarChart from '@/app/components/BarChart';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 
 
 export default function Home() {
   const [values, setValues] = useState<{ [key: string]: number }>({});
   const [secondValues, setSecondValues] = useState<{ [key: string]: number }>({});
-  const [textInput, setTextInput] = useState<string>('');
+  const [labelInput, setLabelInput] = useState<string>('');
   const [title, setTitle] = useState<string>('タイトル未登録');
   const [numberInput, setNumberInput] = useState<number>(1);
   const [secondNumberInput, setSecondNumberInput] = useState<number>(1);
@@ -31,22 +33,21 @@ export default function Home() {
 
   const handleValueSubmit = (e: FormEvent) => {
     e.preventDefault();
-    if (!!textInput.trim()) {
-      setValues({...values, [textInput.trim()]: numberInput});
+    if (!labelInput.trim()) return;
+    setValues({...values, [labelInput.trim()]: numberInput});
 
-      setTextInput('');
-      setNumberInput(0);
-      if (dataNameRef.current && dataNameRef.current.value.trim() != '') {
-        setDataName(dataNameRef.current.value.trim());
-      }
+    setLabelInput('');
+    setNumberInput(0);
+    if (dataNameRef.current && dataNameRef.current.value.trim() != '') {
+      setDataName(dataNameRef.current.value.trim());
+    }
 
-      if (dataCount === 2) {
-        setSecondValues({...secondValues, [textInput.trim()]: secondNumberInput});
-        setSecondNumberInput(0);
-      }
-      if (secondDataNameRef.current && secondDataNameRef.current.value.trim() != '') {
-        setSecondDataName(secondDataNameRef.current.value.trim());
-      }
+    if (dataCount === 2) {
+      setSecondValues({...secondValues, [labelInput.trim()]: secondNumberInput});
+      setSecondNumberInput(0);
+    }
+    if (secondDataNameRef.current && secondDataNameRef.current.value.trim() != '') {
+      setSecondDataName(secondDataNameRef.current.value.trim());
     }
   }
 
@@ -57,8 +58,8 @@ export default function Home() {
     }
   }
 
-  const handleTextInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTextInput(e.target.value);
+  const handleLabelInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLabelInput(e.target.value);
   }
 
   const handleNumberInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -77,46 +78,67 @@ export default function Home() {
     }
   }
 
+  // FIXME::グラフの大きさは、classNameではなく、canvasのwidth, heightに設定されており、そのサイズに合わせてPDFが作成されるので、データのテーブルが小さくなってしまう。
+  const pdhDownloadHandler = () => {
+    const target = document.getElementById('pdf-target');
+    if (target == null) return;
+    html2canvas(target, {
+      width: target.clientWidth,
+      // html2canvas のウィンドウ幅を変更する
+      windowWidth: target.clientWidth,
+      // キャプチャ長さを変更する
+      height: target.clientHeight
+    }).then((canvas) => {
+      const imgData = canvas.toDataURL('image/svg', 1.0);
+      console.log(imgData);
+      let pdf = new jsPDF();
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdfWidth * (canvas.height / canvas.width);
+      pdf.addImage(imgData, 'SVG', 0, 0, pdfWidth, pdfHeight);
+      title != 'タイトル未登録' ? pdf.save(`${title}.pdf`) : pdf.save(`chart.pdf`);
+    });
+  }
+
   return (
     <main className='w-screen'>
       <div className='flex'>
-        <div className='p-2 space-y-2 w-2/3 border-r'>
+        <div className='p-2 space-y-2 w-2/3' id='pdf-target'>
           {style === 1 && <BarChart values={values} title={title} dataName={dataName} dataCount={dataCount} secondValues={secondValues} secondDataName={secondDataName} />}
           {style === 2 && <LineChart values={values} title={title} dataName={dataName} dataCount={dataCount} secondValues={secondValues} secondDataName={secondDataName}/>}
-          <span className='flex space-x-1'>
-          {Object.keys(values).length > 0 &&
-            <div className='border-gray-200 border p-5 w-1/2'>
-              {dataName != ''}<p>{dataName}</p>
-              {Object.entries(values).map(([key, value], index) => (
-                  <div
-                    className={`flex w-full border-gray-300 ${index == Object.entries(values).length - 1 ? 'border'  : 'border-x border-t'}`}
-                    key={index}
-                  >
-                    <p className='border-gray-300 w-3/5 border-r px-1 overflow-hidden overflow-ellipsis'>{key}</p>
-                    <p className='text-right px-1 w-2/5'>{value} {unitOfValue}</p>
-                  </div>
-                ))
-              }
-            </div>
-          }
-          {Object.keys(secondValues).length > 0 &&
-            <div className='border-gray-200 border p-5 w-1/2'>
-              {secondDataName != ''}<p>{secondDataName}</p>
-              {Object.entries(secondValues).map(([key, value], index) => (
-                  <div
-                    className={`flex w-full border-gray-300 ${index == Object.entries(secondValues).length - 1 ? 'border'  : 'border-x border-t'}`}
-                    key={index}
-                  >
-                    <p className='border-gray-300 w-3/5 border-r px-1 overflow-hidden overflow-ellipsis'>{key}</p>
-                    <p className='text-right px-1 w-2/5'>{value} {unitOfValue}</p>
-                  </div>
-                ))
-              }
-            </div>
-          }
-          </span>
+          <div className='flex space-x-1'>
+            {Object.keys(values).length > 0 &&
+              <div className='border-gray-200 border p-5 w-1/2'>
+                {dataName != ''}<p>{dataName}</p>
+                {Object.entries(values).map(([key, value], index) => (
+                    <div
+                      className={`flex w-full border-gray-300 ${index == Object.entries(values).length - 1 ? 'border'  : 'border-x border-t'}`}
+                      key={index}
+                    >
+                      <p className='border-gray-300 w-3/5 border-r px-1 overflow-hidden overflow-ellipsis'>{key}</p>
+                      <p className='text-right px-1 w-2/5'>{value} {unitOfValue}</p>
+                    </div>
+                  ))
+                }
+              </div>
+            }
+            {Object.keys(secondValues).length > 0 &&
+              <div className='border-gray-200 border p-5 w-1/2'>
+                {secondDataName != ''}<p>{secondDataName}</p>
+                {Object.entries(secondValues).map(([key, value], index) => (
+                    <div
+                      className={`flex w-full border-gray-300 ${index == Object.entries(secondValues).length - 1 ? 'border'  : 'border-x border-t'}`}
+                      key={index}
+                    >
+                      <p className='border-gray-300 w-3/5 border-r px-1 overflow-hidden overflow-ellipsis'>{key}</p>
+                      <p className='text-right px-1 w-2/5'>{value} {unitOfValue}</p>
+                    </div>
+                  ))
+                }
+              </div>
+            }
+          </div>
         </div>
-        <div className='p-2 space-y-3 w-1/3'>
+        <div className='p-2 space-y-3 w-1/3 border-l'>
           {title == 'タイトル未登録' ? (
             <div>
               <p className='text-sm font-bold mb-1'>タイトル</p>
@@ -200,7 +222,7 @@ export default function Home() {
                   <input type='number' step='0.1' id='firstNumber' className='border-gray-300 border text-right outline-none block w-1/2' value={numberInput} onChange={handleNumberInputChange} />
                 </div>
                 <div className='mb-2'>
-                  <input type='text' className='border-gray-300 border px-1 outline-none w-full block' value={textInput} onChange={handleTextInputChange} placeholder='ラベル名' required />
+                  <input type='text' className='border-gray-300 border px-1 outline-none w-full block' value={labelInput} onChange={handleLabelInputChange} placeholder='ラベル名' required />
                 </div>
                 <span className='flex justify-center mt-2'>
                   <button type='submit' className='border-gray-300 border px-2 w-full text-center'>値追加</button>
@@ -228,7 +250,7 @@ export default function Home() {
                   </span>
                 </div>
                 <div className='mb-2'>
-                  <input type='text' className='border-gray-300 border px-1 outline-none w-full block' value={textInput} onChange={handleTextInputChange} placeholder='ラベル名' required />
+                  <input type='text' className='border-gray-300 border px-1 outline-none w-full block' value={labelInput} onChange={handleLabelInputChange} placeholder='ラベル名' required />
                 </div>
                 <span className='flex justify-center mt-2'>
                   <button type='submit' className='border-gray-300 border px-2 w-full text-center'>値追加</button>
@@ -236,6 +258,9 @@ export default function Home() {
               </form>
             )}
           </div>
+          <button type='button' onClick={pdhDownloadHandler} className='border p-2'>
+            PDFファイルをダウンロード
+          </button>
         </div>
       </div>
     </main>
